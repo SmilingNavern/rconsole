afbb7bbbf1cda29a451a60469c4b5503bbca9820
Alexander Danilov <livingdeadzerg@yandex.ru>
2016-01-05 22:19:05 +0300

    readchar implemented

--- src/main.rs
+++ src/main.rs
@@ -12,18 +12,22 @@ fn read_password() -> Result<String,Error> {
     Ok(s)
 }
 
-fn readchar(stream: TcpStream) {
-    let mut buffer: [u8] = [0; 1];
-    let _ = stream.read(&mut buffer);
+fn readchar(stream: &TcpStream) -> String {
+    let mut rstream = BufReader::new(stream.try_clone().unwrap());
+    let mut buffer = String::with_capacity(2);
+    let _ = rstream.read_line(&mut buffer).unwrap();
+    buffer.trim().to_string()
 }
 
-fn do_sysreq(stream: TcpStream, key: char, sysreq_fh: File) -> Option<int> {
+fn do_sysreq(stream: &mut TcpStream, key: char, sysreq_fh: File) -> Result<(), Error> {
     if !key.is_alphabetic() {
         let _ = stream.write(b"Key out of range\n");
-        return None; 
+        return Ok(()); 
     } 
 
-    let _ = stream.write(b"Send {} to sysreq? (y/n)\n", key);
+    let _ = stream.write(format!("Send {} to sysreq? (y/n)\n", key).as_bytes());
+
+    Ok(())
 }
 
 fn handle_client(mut stream: TcpStream, password: &String) {
@@ -34,7 +38,12 @@ fn handle_client(mut stream: TcpStream, password: &String) {
     let s1 = password.trim();
     let s2 = buffer.trim();
 
-    print!("{} and {}", s1, s2);
+    print!("{} and {}\n", s1, s2);
+
+    let _ = stream.write(b"PUT A CHAR: ");
+    let c = readchar(&stream);
+
+    let _ = stream.write(format!("{}",c).as_bytes());
 
     if s1 == s2 {
         let _ = stream.write(b"Hello, world\n");
